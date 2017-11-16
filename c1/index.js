bac0e95d0cdb010a483048d7fcba5402b3442c5b
mija80 <32924047+mija80@users.noreply.github.com>
2017-11-16 13:36:10 +0100

    1

---
+++ c1/index.js
@@ -0,0 +1,27 @@
+console.log('Hello world!');
+
+
+var ime = "Bojan"; // string (niza od karakteri)
+var godini = 35; // number
+var visina = 182.5; // number
+var temperatura = -22.5; // number
+var classEnded =true; // boolean (ova se tipovi na promenlivi)
+
+console.log(ime); 
+console.log(godini);
+console.log(visina);
+console.log(temperatura);
+console.log(classEnded);
+
+
+godinaNaRagjanje = 1982; // camel case style
+
+godina_na_ragjanje = 1982; // snake case style
+ 
+ godinanaragjanje = na 1982; //  BAD CASE ne go pravite
+
+ 
+
+//"....."
+//'....'
+//....
