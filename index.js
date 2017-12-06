8394d4751cc8770738e293cd7090d41a0f0fc2af
mija80 <32924047+mija80@users.noreply.github.com>
2017-12-07 00:46:08 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+var niza5 = [2, 5, 3, 2, 3, 3, 5, 1, 1, 4, 4, 2, 4, 2, 2, 4];
+var a =  0 ;
+
++ //sekojTretParenBroj(niza5); // console.log
+ +// 4
+ +// 4
+ +
+ +//zbirNaNeparniBroevi(niza5); // return
+ +// 21
+ +
+ +//prosek(niza5); // return
+ +// 2.9375 
+function sekojTretParenBroj(niza){
+	var a = 0;
+for (var i = 0; i < niza5.length; i ++)	{
+	n = niza5[i];
+	if (n % 2 == 0){
+	if (niza[i] % 2 == 0){
+		    a = a + 1;
+		    if(b==3){
+		    console.log(niza[i]);	
+		    }
+			
+			
+		};
+	}
+
+}
+}
+
+
